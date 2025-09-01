bb24a5d994959bf89400646588e3adc681f9f57d
dnnsptri <hi@dennispetri.nl>
2025-09-01 16:10:50 +0200

    Add Google Analytics tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-21WM6EDG2N"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-21WM6EDG2N');
+            `
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 } 
